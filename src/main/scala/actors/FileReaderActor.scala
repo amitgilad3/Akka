8b621c70ffa97df53b421ddc52cf83659e633f48
amit <amit.gilad@sizmek.com>
2015-11-22 01:04:31 +0200

    created actor for reading files + modified actor to recieve actor ref

--- src/main/scala/actors/FileReaderActor.scala
+++ src/main/scala/actors/FileReaderActor.scala
@@ -1,6 +1,7 @@
 package actors
 
-import akka.actor.Actor
+import _root_.models.HandleResponse
+import akka.actor.{ActorRef, Actor}
 import akka.event.Logging
 
 import scala.io.Source
@@ -11,12 +12,13 @@ import scala.io.Source
 
 case class ReadFile(fileLocation : String)
 
-class FileReaderActor extends Actor{
+class FileReaderActor(actor : ActorRef) extends Actor{
   val log = Logging(context.system, this)
+  var responseHandlerActor: ActorRef = actor
   def receive: Receive = {
     case ReadFile(fileLocation : String)  => {
       val source = Source.fromFile(fileLocation).getLines().flatMap(_.split("\\W+"))
-
+      //responseHandlerActor ! HandleResponse(response.body)
       log.info("finished reading file")
     }
 
