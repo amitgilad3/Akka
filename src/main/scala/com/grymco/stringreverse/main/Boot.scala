b76fc14fccf889e49f2cb8acff07aa4794276344
amit <amit.gilad@sizmek.com>
2015-11-22 00:04:43 +0200

    new project

---
+++ src/main/scala/com/grymco/stringreverse/main/Boot.scala
@@ -0,0 +1,20 @@
+package com.grymco.stringreverse.main
+
+import akka.actor.{ActorSystem, Props}
+import akka.io.IO
+import com.grymco.stringreverse.actors.StringServiceActor
+import com.typesafe.scalalogging.Logger
+import org.slf4j.LoggerFactory
+import spray.can.Http
+import akka.pattern.ask
+import akka.util.Timeout
+import scala.concurrent.duration._
+
+/**
+ * Created by Amit on 21/11/2015.
+ */
+object Boot extends App{
+
+
+
+}
